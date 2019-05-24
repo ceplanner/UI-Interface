f4878d96e4338b4741e2651410dfeb0764454d5a
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-24 08:04:50 +0100

    convert about us compoenent to use foreach to fill data

--- js/aboutComponent.js
+++ js/aboutComponent.js
@@ -1,3 +1,48 @@
+const membersArr = [
+  {
+    h2Content: "Nate Boyette",
+    h3Content: "Team Lead",
+    paraContent:
+      "Nate is the team lead, he keeps track of the day activities. He is incharge of making sure the developers are on track to give you the product you deserve",
+    imageSrc: "./assets/images/nate.jpg"
+  },
+
+  {
+    h2Content: "Kelechi Ogbonna",
+    h3Content: "UI Developer",
+    paraContent:
+      "Kelechi is part of the team working hard to build build an interface to give you the marketing page you deserve.",
+    imageSrc: "./assets/images/kelechi.jpg"
+  },
+  {
+    h2Content: "George Adams",
+    h3Content: "UI Developer",
+    paraContent:
+      "George is part of the team working hard to build build an interface to give you the marketing page you deserve.",
+    imageSrc: "./assets/images/george.jpg"
+  },
+  {
+    h2Content: "Firealem Eko",
+    h3Content: "Frontend developer",
+    paraContent:
+      "firealem is part of the team dedicated to building the application tools you interact with when you use corporate event planner. A team dedicated to building amazing functionalities for your needs.",
+    imageSrc: "./assets/images/firealempic.jpg"
+  },
+  {
+    h2Content: "April Ding",
+    h3Content: "UX designer",
+    paraContent:
+      "April is part of the team dedicated to creating a seamless user experience, focusing on making the website user friendly, so you could have the best experience.",
+    imageSrc: "./assets/images/april.jpg"
+  },
+  {
+    h2Content: "Dayton Stienmeyer",
+    h3Content: "Backend Developer",
+    paraContent:
+      "Dayton is part of the team dedicated to building the functionalities we provide at corporate event planner. A team dedicated to building amazing functionalities for your needs.",
+    imageSrc: "./assets/images/dayton.png"
+  }
+];
 
 class About {
   constructor(data) {
@@ -41,51 +86,11 @@ class About {
   }
 }
 
-const nate = new About({
-  h2Content: "Nate Boyette",
-  h3Content: "Team Lead",
-  paraContent:
-    "Nate is the team lead, he keeps track of the day activities. He is incharge of making sure the developers are on track to give you the product you deserve",
-  imageSrc: "./assets/images/nate.jpg"
+const memebers = membersArr.forEach(member => {
+  new About({
+    h2Conent: member.h2Content,
+    h3Content: member.h3Content,
+    paraContent: member.paraContent,
+    imageSrc: member.imageSrc
+  });
 });
-
-const kelechi = new About({
-    h2Content: "Kelechi Ogbonna",
-    h3Content: "UI Developer",
-    paraContent:
-      "Kelechi is part of the team working hard to build build an interface to give you the marketing page you deserve.",
-    imageSrc: "./assets/images/kelechi.jpg"
-  });
-
-  const george = new About({
-    h2Content: "George Adams",
-    h3Content: "UI Developer",
-    paraContent:
-    "George is part of the team working hard to build build an interface to give you the marketing page you deserve.",
-    imageSrc: "./assets/images/george.jpg"
-  });
-
-  const firealem = new About({
-    h2Content: "Firealem Eko",
-    h3Content: "Frontend developer",
-    paraContent:
-    "firealem is part of the team dedicated to building the application tools you interact with when you use corporate event planner. A team dedicated to building amazing functionalities for your needs.",
-    imageSrc: "./assets/images/firealempic.jpg"
-  });
-
-  const April = new About({
-    h2Content: "April Ding",
-    h3Content: "UX designer",
-    paraContent:
-      "April is part of the team dedicated to creating a seamless user experience, focusing on making the website user friendly, so you could have the best experience.",
-    imageSrc: "./assets/images/april.jpg"
-  });
-
-  const Dayton = new About({
-    h2Content: "Dayton Stienmeyer",
-    h3Content: "Backend Developer",
-    paraContent:
-    "Dayton is part of the team dedicated to building the functionalities we provide at corporate event planner. A team dedicated to building amazing functionalities for your needs.",
-    imageSrc: "./assets/images/dayton.png"
-  });
-
